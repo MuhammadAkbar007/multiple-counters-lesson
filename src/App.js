1776cf47f4fb6258948695d22b70068e65c5e81b
MuhammadAkbar <fotismenos007@mail.ru>
2022-10-07 19:06:39 +0500

    storm

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [counters, setCounters] = useState([12, 27, 88])
+
+    function addCounter() {
+        counters.push(0)
+        setCounters(counters => [...counters])
+    }
+
+    function changeCounter(index, value) {
+        let temp = counters[index]
+        temp = temp + value
+        counters[index] = temp
+        setCounters(counters => [...counters])
+    }
+
+    return (
+        <div className="App">
+            <div className="container mt-5">
+                <div className="row">
+                    <div className="col-md-12">
+                        <button className={"btn btn-success"} onClick={addCounter}>Add Counter</button>
+                        {
+                            counters.map((item, index) =>
+                                <div key={index}>
+                                    <button className="btn btn-info mt-1 mx-1" onClick={() => changeCounter(index, 1)}>+
+                                    </button>
+                                    {item}
+                                    <button className="btn btn-danger mt-1 mx-1" onClick={() => changeCounter(index, -1)}>-
+                                    </button>
+                                </div>)
+                        }
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
